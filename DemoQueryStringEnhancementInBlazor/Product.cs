44b2d3e6179573d8d5fde2a2ee472d3c6c9df3e7
Netcode-Hub <Rastinga1995@hotmail.com>
2023-12-02 16:12:33 +0000

    Add project files.

---
+++ DemoQueryStringEnhancementInBlazor/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoQueryStringEnhancementInBlazor
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
